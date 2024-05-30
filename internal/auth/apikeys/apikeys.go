8ff2403fe974e0194024a0cd6b0034eeb738c658
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-05-29 22:56:40 -0700

    Update the API key commands to use the new path

--- internal/auth/apikeys/apikeys.go
+++ internal/auth/apikeys/apikeys.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/llm-operator/cli/internal/auth/org"
+	"github.com/llm-operator/cli/internal/auth/project"
 	ihttp "github.com/llm-operator/cli/internal/http"
 	"github.com/llm-operator/cli/internal/runtime"
 	"github.com/llm-operator/cli/internal/ui"
@@ -15,7 +17,7 @@ import (
 )
 
 const (
-	path = "/users/api_keys"
+	pathPattern = "/organizations/%s/projects/%s/api-keys"
 )
 
 // Cmd is the root command for apikeys.
@@ -34,56 +36,79 @@ func Cmd() *cobra.Command {
 
 func createCmd() *cobra.Command {
 	var (
-		name string
+		name         string
+		orgTitle     string
+		projectTitle string
 	)
 	cmd := &cobra.Command{
 		Use:  "create",
 		Args: cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return create(cmd.Context(), name)
+			return create(cmd.Context(), name, orgTitle, projectTitle)
 		},
 	}
 	cmd.Flags().StringVar(&name, "name", "", "Name of the API key")
+	cmd.Flags().StringVarP(&orgTitle, "organization-title", "o", "", "Title of the organization. The organization in the current context is used if not specified.")
+	cmd.Flags().StringVarP(&projectTitle, "project-title", "p", "", "Title of the project. The project in the current context is used if not specified.")
 	_ = cmd.MarkFlagRequired("name")
 	return cmd
 }
 
 func listCmd() *cobra.Command {
-	return &cobra.Command{
+	var (
+		orgTitle     string
+		projectTitle string
+	)
+	cmd := &cobra.Command{
 		Use:  "list",
 		Args: cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return list(cmd.Context())
+			return list(cmd.Context(), orgTitle, projectTitle)
 		},
 	}
+	cmd.Flags().StringVarP(&orgTitle, "organization-title", "o", "", "Title of the organization. The organization in the current context is used if not specified.")
+	cmd.Flags().StringVarP(&projectTitle, "project-title", "p", "", "Title of the project. The project in the current context is used if not specified.")
+	return cmd
 }
 
 func deleteCmd() *cobra.Command {
 	var (
-		name string
+		name         string
+		orgTitle     string
+		projectTitle string
 	)
 	cmd := &cobra.Command{
 		Use:  "delete",
 		Args: cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return delete(cmd.Context(), name)
+			return delete(cmd.Context(), name, orgTitle, projectTitle)
 		},
 	}
 	cmd.Flags().StringVar(&name, "name", "", "Name of the API key")
+	cmd.Flags().StringVarP(&orgTitle, "organization-title", "o", "", "Title of the organization. The organization in the current context is used if not specified.")
+	cmd.Flags().StringVarP(&projectTitle, "project-title", "p", "", "Title of the project. The project in the current context is used if not specified.")
 	_ = cmd.MarkFlagRequired("name")
 	return cmd
 }
 
-func create(ctx context.Context, name string) error {
+func create(ctx context.Context, name, orgTitle, projectTitle string) error {
 	env, err := runtime.NewEnv(ctx)
 	if err != nil {
 		return err
 	}
 
+	org, project, err := findOrgAndProject(env, orgTitle, projectTitle)
+	if err != nil {
+		return err
+	}
+
 	req := &uv1.CreateAPIKeyRequest{
-		Name: name,
+		Name:           name,
+		OrganizationId: org.Id,
+		ProjectId:      project.Id,
 	}
 	var resp uv1.APIKey
+	path := fmt.Sprintf(pathPattern, org.Id, project.Id)
 	if err := ihttp.NewClient(env).Send(http.MethodPost, path, &req, &resp); err != nil {
 		return err
 	}
@@ -92,15 +117,24 @@ func create(ctx context.Context, name string) error {
 	return nil
 }
 
-func list(ctx context.Context) error {
+func list(ctx context.Context, orgTitle, projectTitle string) error {
 	env, err := runtime.NewEnv(ctx)
 	if err != nil {
 		return err
 	}
 
-	var req uv1.ListAPIKeysRequest
+	org, project, err := findOrgAndProject(env, orgTitle, projectTitle)
+	if err != nil {
+		return err
+	}
+
+	req := &uv1.ListAPIKeysRequest{
+		OrganizationId: org.Id,
+		ProjectId:      project.Id,
+	}
 	var resp uv1.ListAPIKeysResponse
-	if err := ihttp.NewClient(env).Send(http.MethodGet, path, &req, &resp); err != nil {
+	path := fmt.Sprintf(pathPattern, org.Id, project.Id)
+	if err := ihttp.NewClient(env).Send(http.MethodGet, path, req, &resp); err != nil {
 		return err
 	}
 
@@ -116,13 +150,18 @@ func list(ctx context.Context) error {
 	return nil
 }
 
-func delete(ctx context.Context, name string) error {
+func delete(ctx context.Context, name, orgTitle, projectTitle string) error {
 	env, err := runtime.NewEnv(ctx)
 	if err != nil {
 		return err
 	}
 
-	key, found, err := findKeyByName(ctx, env, name)
+	org, project, err := findOrgAndProject(env, orgTitle, projectTitle)
+	if err != nil {
+		return err
+	}
+
+	key, found, err := findKeyByName(ctx, env, name, org, project)
 	if err != nil {
 		return err
 	}
@@ -131,9 +170,12 @@ func delete(ctx context.Context, name string) error {
 	}
 
 	req := &uv1.DeleteAPIKeyRequest{
-		Id: key.Id,
+		Id:             key.Id,
+		OrganizationId: org.Id,
+		ProjectId:      project.Id,
 	}
 	var resp uv1.DeleteAPIKeyResponse
+	path := fmt.Sprintf(pathPattern, org.Id, project.Id)
 	if err := ihttp.NewClient(env).Send(http.MethodDelete, fmt.Sprintf("%s/%s", path, key.Id), &req, &resp); err != nil {
 		return err
 	}
@@ -143,9 +185,39 @@ func delete(ctx context.Context, name string) error {
 	return nil
 }
 
-func findKeyByName(ctx context.Context, env *runtime.Env, name string) (*uv1.APIKey, bool, error) {
-	var req uv1.ListAPIKeysRequest
+func findOrgAndProject(env *runtime.Env, orgTitle, projectTitle string) (*uv1.Organization, *uv1.Project, error) {
+	org, found, err := org.FindOrgByTitle(env, orgTitle)
+	if err != nil {
+		return nil, nil, err
+	}
+	if !found {
+		return nil, nil, fmt.Errorf("organization not found")
+	}
+
+	project, found, err := project.FindProjectByTitle(env, projectTitle, orgTitle)
+	if err != nil {
+		return nil, nil, err
+	}
+	if !found {
+		return nil, nil, fmt.Errorf("project not found in organization")
+	}
+
+	return org, project, nil
+}
+
+func findKeyByName(
+	ctx context.Context,
+	env *runtime.Env,
+	name string,
+	org *uv1.Organization,
+	project *uv1.Project,
+) (*uv1.APIKey, bool, error) {
+	req := &uv1.ListAPIKeysRequest{
+		OrganizationId: org.Id,
+		ProjectId:      project.Id,
+	}
 	var resp uv1.ListAPIKeysResponse
+	path := fmt.Sprintf(pathPattern, org.Id, project.Id)
 	if err := ihttp.NewClient(env).Send(http.MethodGet, path, &req, &resp); err != nil {
 		return nil, false, err
 	}
